156df392303f277d3ec923dafb9e132322b1dc17
Nami0721 <nmhy72@gmail.com>
2017-05-10 11:58:39 +0200

    May10 End of day

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,4 @@ var toggleSlide = function(){
   $(".product .left img.active").removeClass()
    .next().add(".product .left img:first").last().addClass("active");
 }
-setInterval(toggleSlide, 5000);
+setInterval(toggleSlide, 2500);

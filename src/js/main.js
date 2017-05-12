34b31c67c57cd8688f2c095a2c37bda8e52279d3
Nami0721 <nmhy72@gmail.com>
2017-05-12 12:14:26 +0200

    May12 End of day

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,9 @@ var toggleSlide = function(){
    .next().add(".product .left img:first").last().addClass("active");
 }
 setInterval(toggleSlide, 2500);
+
+
+$("#buttonTop").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
